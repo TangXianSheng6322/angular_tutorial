5df9d8360792f0e9994bc00cb24538c0a03d7eee
Nick <n.sakharov@icloud.com>
2025-04-10 20:34:50 +0700

    Add some css

--- NYResolutions25/src/app/resolution-list-item/resolution-list-item.component.ts
+++ NYResolutions25/src/app/resolution-list-item/resolution-list-item.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'resolution-list-item',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './resolution-list-item.component.html',
   styleUrl: './resolution-list-item.component.css',
 })
@@ -10,6 +10,11 @@ export class ResolutionListItemComponent {
   @Input() ResolutionText!: string;
   @Input() fulfilled!: boolean;
   @Output() fulfilledChange = new EventEmitter<boolean>();
+
+  get cssClasses() {
+    // return this.fulfilled ? ['strikeout', 'text-muted'] : [];
+    return { 'strikeout text-muted': this.fulfilled };
+  }
 
   toggleFulfilled() {
     this.fulfilled = !this.fulfilled;
