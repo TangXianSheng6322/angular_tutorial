6cbe0b9130094d19ac4fb77a334a71e9057ecf65
Nick <n.sakharov@icloud.com>
2025-04-13 17:13:21 +0700

    Providing Options with  HTTP request

--- NYResolutions25/src/app/app.component.ts
+++ NYResolutions25/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, importProvidersFrom, OnInit } from '@angular/core';
 import { ResolutionItem } from '../shared/models/resolutionItem';
 import { FormsModule } from '@angular/forms';
 import { ResolutionListComponent } from './resolution-list/resolution-list.component';
@@ -6,6 +6,7 @@ import { AddResolutionFormComponent } from './add-resolution-form/add-resolution
 import { ResolutionFilterComponent } from './resolution-filter/resolution-filter.component';
 import { EventService } from './../shared/services/EventService';
 import { ResolutionService } from './resolution.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -21,11 +22,17 @@ import { ResolutionService } from './resolution.service';
 export class AppComponent implements OnInit {
   items!: ResolutionItem[];
 
-  filter: any;
+  // = [
+  //   new ResolutionItem('Learn Angular'),
+  //   new ResolutionItem('Get Coffee', true),
+  //   new ResolutionItem('Find grass that cuts itself'),
+  // ];
+
   title = 'NYResolutions25';
+  filter: any;
 
   constructor(
-    private events: EventService,
+    events: EventService,
     private resolutionService: ResolutionService,
   ) {
     events.listen('removeResolution', (resolution: any) => {
--- NYResolutions25/src/app/resolution.service.ts
+++ NYResolutions25/src/app/resolution.service.ts
@@ -1,13 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { ResolutionItem } from '../shared/models/resolutionItem';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ResolutionService {
   constructor(private http: HttpClient) {}
+  // private http = inject(HttpClient);
+
+  private getStandartOptions(): any {
+    return { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+  }
 
   getResolutions() {
-    return this.http.get('./assets/resolutions.json');
+    let options = this.getStandartOptions();
+    options.params = new HttpParams({
+      fromObject: { format: 'json' },
+    });
+    let url: string = 'assets/resolutions.json';
+    return this.http.get(url, options);
+  }
+
+  addResolution(resolution: ResolutionItem) {
+    let options = this.getStandartOptions();
+    let url: string = 'assets/resolutions.json';
+    options.headers = options.headers.set(
+      'Authorization',
+      'value-need-for-authorization',
+    );
+    this.http.post(url, resolution, options);
   }
 }
